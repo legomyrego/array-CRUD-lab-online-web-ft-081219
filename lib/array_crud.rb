baa681937200d4479e1696ec54d1193e13d1a33a
Mark Rego <rego121@gmail.com>
2019-08-14 18:19:00 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,12 +3,14 @@ def create_an_empty_array
 end
 
 def create_an_array
-  arr = ["mango","watermelon", "melon"]
+  arr = ["mango","watermelon", "melon","apple"]
 
 end
 
 def add_element_to_end_of_array(array, element)
   array << element
+
+  
 
 end
 
